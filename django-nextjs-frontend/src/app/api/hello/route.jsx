c844a6683e265ee550488a39764b5924e4070f9e
chvaldez10 <christianvaldez021@gmail.com>
2024-11-27 19:59:21 -0700

    remove nextjs project

--- django-nextjs-frontend/src/app/api/hello/route.jsx
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import ApiProxy from "../proxy";
-import { DJANGO_API_ENDPOINT } from "@/config/defaults";
-
-
-export async function GET(request){
-    const data = {apiEndpoint: DJANGO_API_ENDPOINT}
-    return NextResponse.json(data, {status: 200})
-}
--- django-nextjs-frontend/src/config/defaults.jsx
+++
@@ -1,2 +0,0 @@
-export const DJANGO_BASE_URL=process.env.DJANGO_BASE_URL
-export const DJANGO_API_ENDPOINT=`${DJANGO_BASE_URL}/api`

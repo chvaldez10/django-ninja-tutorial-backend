0f019c14dfdf33167be9944b224bc2e27b6acce9
chvaldez10 <christianvaldez021@gmail.com>
2024-11-29 19:26:51 -0700

    migrate user desc

--- src/waitlists/models.py
+++ src/waitlists/models.py
@@ -8,3 +8,4 @@ class WaitlistEntry(models.Model):
     email = models.EmailField()
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    description = models.TextField(blank=True, null=True)
